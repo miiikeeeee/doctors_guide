3aa9ad3f4113091f858db8bce25edfb827c4213e
Michael Makaryshyn <mi5hqaaaaov@gmail.com>
2023-11-10 23:13:57 +0200

    tst

--- src/components/GermanyMap/GermanyMap.jsx
+++ src/components/GermanyMap/GermanyMap.jsx
@@ -1,6 +1,5 @@
 import { useState, useEffect } from "react";
 import { MapContainer, TileLayer, Polygon } from "react-leaflet";
-import '/leaflet/dist/leaflet.css';
 
 import { statesData } from "./data";
 
